7e649d447e10c44319f82bd7dcdbea29d3250d9c
Emily Thomas <ertgd8@mail.umkc.edu>
2016-05-03 19:05:54 -0500

    Updated Files
    Files now mostly complete.

--- DB/src/Example.java
+++ DB/src/Example.java
@@ -1,9 +1,225 @@
-public class Example {
+package db;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Gateway gateway=new Gateway();
+import db.Gateway;
+
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Example {
+	private Vector<Vector<Object>> companies=new Vector<Vector<Object>>();
+	private Vector<Vector<Vector<Object>>> products=new Vector<Vector<Vector<Object>>>();
+	private Vector<Vector<Object>> customers=new Vector<Vector<Object>>();
+	public Example() throws SQLException{
+		companies=Gateway.selectCompanies();
+		customers=Gateway.selectCustomers();
+	}
+	//good economy, increase budget limit, decrease prices, increase quantity
+	public void goodEconomy() throws SQLException{
+		//do queries to update budget, price and quantity
+		//update prices
+		//update budget
+		//for each customer
+		//	for each product set, rank them according to customer info and have the customer pick from each set
+		//get the products for each company
 		
+		Vector<String> product_types=new Vector<String>();
+		product_types.add("ToiletPaper");
+		product_types.add("Cereal");
+		product_types.add("Vacuum");
+		product_types.add("MobilePhone");
+		product_types.add("TV");
+		//products are grouped by product type, so the customer can pick one of each type
+		products=Gateway.selectProducts(product_types);
+		
+		//first slash the price of each product
+		int percent_slash=2;
+		for(int j=0;j<products.size();++j){
+			//for every set of products in a product type
+			for(int k=0;k<products.get(j).size();++k){
+				Vector<Object> size=products.get(j).get(k);
+				int holder_id=(int)size.get(1);
+				Gateway.updateProductPrice(holder_id, percent_slash);
+			}
+		}
+		
+		for(int i=0;i<customers.size();++i){
+			int customer_id=(int)customers.get(i).get(0);
+			//raise the budget of this customer
+			int customer_budget=(int)customers.get(i).get(1);
+			Gateway.updateCustomerBudget(customer_id, 30, customer_budget);
+		}
+		shop();
+	}
+	
+	public void shop() throws SQLException{
+		for(int i=0;i<customers.size();++i){
+			int customer_id=(int)customers.get(i).get(0);
+			int customer_budget=(int)customers.get(i).get(1);
+			for(int j=0;j<products.size();++j){
+				//for every set of products in a product type
+				Vector<Integer> top_rank_product=new Vector<Integer>();
+				for(int k=0;k<products.get(j).size();++k){
+					//for every product in a set
+					//judge their ranking and tally them up to make the best choice
+					//vector first entry is holder id, second is price, third is rating and fourth is final ranking
+					//initialize the top rank product
+					top_rank_product.add(0,0);
+					top_rank_product.add(1,100000000);
+					top_rank_product.add(2,0);
+					
+					int holder_id=(int)products.get(j).get(k).get(1);
+					int new_price=(int)products.get(j).get(k).get(2);
+					if(new_price<=customer_budget){
+						int rating=(int)products.get(j).get(k).get(4);
+						if(rating>top_rank_product.get(1)&&new_price<top_rank_product.get(1)){
+							//replace other product with new
+							top_rank_product.add(0, holder_id);
+							top_rank_product.add(1, new_price);
+							top_rank_product.add(2, rating);
+						}
+					}
+				}
+				buyProduct(top_rank_product,customer_id,customer_budget);
+			}
+		}
 	}
 
+	
+	private void buyProduct(Vector<Integer> top_rank_product, int customer_id, int budget) throws SQLException{
+		int company_id=top_rank_product.get(0);
+		int price=top_rank_product.get(1); //add to company profit (minus taxes)
+		int neg_price=-price; //subtract from customer budget
+		int profit=0;
+		for(int l=0;l<companies.size();++l){
+			if((int)companies.get(l).get(2)==top_rank_product.get(0)){
+				profit=(int) companies.get(l).get(2);
+			}
+		}
+		Gateway.updateCustomerBudget(company_id,neg_price,budget);
+		Gateway.updateCompanyProfit(company_id,price,profit);
+	}
+	//bad economy, decrease budget limit, increase prices, decrease quantity
+	public void badEconomy() throws SQLException{
+		//do queries to update budget, price and quantity
+				//update prices
+				//update budget
+				//for each customer
+				//	for each product set, rank them according to customer info and have the customer pick from each set
+				//get the products for each company
+				
+				Vector<String> product_types=new Vector<String>();
+				product_types.add("ToiletPaper");
+				product_types.add("Cereal");
+				product_types.add("Vacuum");
+				product_types.add("MobilePhone");
+				product_types.add("TV");
+				//products are grouped by product type, so the customer can pick one of each type
+				products=Gateway.selectProducts(product_types);
+				
+				//first slash the price of each product
+				int percent_slash=1/2;
+				for(int j=0;j<products.size();++j){
+					//for every set of products in a product type
+					for(int k=0;k<products.get(j).size();++k){
+						Vector<Object> size=products.get(j).get(k);
+						int holder_id=(int)size.get(1);
+						Gateway.updateProductPrice(holder_id, percent_slash);
+					}
+				}
+				
+				for(int i=0;i<customers.size();++i){
+					int customer_id=(int)customers.get(i).get(0);
+					//raise the budget of this customer
+					int customer_budget=(int)customers.get(i).get(1);
+					Gateway.updateCustomerBudget(customer_id, -30, customer_budget);
+				}
+			shop();
+	}
+	
+	public static void main(String[] args) {
+		try{
+			Gateway.connect();
+			Gateway.dropTable("COMPANY");
+			Gateway.dropTable("PRODUCT");
+			Gateway.dropTable("CUSTOMER");
+			Gateway.createTables();
+			
+			Gateway.addCompany(112234, "Wally-World", 5);
+			Gateway.addCompany(123435, "Price Chomper", 0);
+			Gateway.addCompany(452429, "ArrowMart", 0);
+			Gateway.addCompany(887478, "Belongs-to-Us", 0);
+			Gateway.addCompany(127476, "Penny Store", 0);
+			
+			//Gateway.updateCompanyProfit(112234, 25);
+			
+			Gateway.addCustomer(128947284, 1230);
+			Gateway.addCustomer(234234098, 2399);
+			Gateway.addCustomer(989234882, 2312);
+			Gateway.addCustomer(348578757, 8786);
+			Gateway.addCustomer(328472483, 3245);
+			
+			Gateway.addProduct("Shakin", 112234, 13, "ToiletPaper", 5);
+			Gateway.addProduct("Popmallows", 112234, 4, "Cereal", 4);
+			Gateway.addProduct("Tornado", 112234, 24, "Vacuum", 3);
+			Gateway.addProduct("Raspberry", 112234, 400, "MobilePhone", 3);
+			Gateway.addProduct("HDDelux", 112234, 1880, "TV", 4);
+			
+			Gateway.addProduct("ComfortWipe", 123435, 10, "ToiletPaper", 2);
+			Gateway.addProduct("ChocoPuffs", 123435, 4, "Cereal", 4);
+			Gateway.addProduct("Cleanerr", 123435, 45, "Vacuum", 4);
+			Gateway.addProduct("FlipMaster", 123435, 560, "MobilePhone", 3);
+			Gateway.addProduct("Sungsam", 123435, 877, "TV", 5);
+			
+			Gateway.addProduct("Bountiful", 452429, 14, "ToiletPaper", 3);
+			Gateway.addProduct("Pranx", 452429, 9, "Cereal", 4);
+			Gateway.addProduct("Sweeptastic", 452429, 56, "Vacuum", 2);
+			Gateway.addProduct("On-the-Go", 452429, 456, "MobilePhone", 5);
+			Gateway.addProduct("ScreenCenter", 452429, 2000, "TV", 4);
+			
+			Gateway.addProduct("CarePaper", 887478, 16, "ToiletPaper", 4);
+			Gateway.addProduct("ToastSquares", 887478, 3, "Cereal", 4);
+			Gateway.addProduct("Whirlwind", 887478, 34, "Vacuum", 5);
+			Gateway.addProduct("uChat", 887478, 569, "MobilePhone", 3);
+			Gateway.addProduct("ColorMaster", 887478, 2999, "TV", 4);
+			
+			Gateway.addProduct("OnaCloud", 127476, 16, "ToiletPaper", 5);
+			Gateway.addProduct("BreakfastOats", 127476, 1, "Cereal", 4);
+			Gateway.addProduct("FloorHero", 127476, 33, "Vacuum", 3);
+			Gateway.addProduct("iTalk", 127476, 670, "MobilePhone", 3);
+			Gateway.addProduct("VisualMight", 127476, 949, "TV", 2);
+				
+			
+			
+			Example example=new Example();
+			example.goodEconomy();
+			
+			Vector<Vector<Object>> companies=Gateway.selectCompanies();
+			for(int i=0;i<companies.size();++i){
+				for(int j=0;j<companies.get(i).size();++j){
+					System.out.println(companies.get(i).get(j));
+				}
+			}
+			
+			Vector<Vector<Object>> customers=Gateway.selectCustomers();
+			for(int i=0;i<customers.size();++i){
+				for(int j=0;j<customers.get(i).size();++j){
+					System.out.println(customers.get(i).get(j));
+				}
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
+
+/*
+ QUESTIONS:
+ What is the most popular product?
+ What is the most popular company?
+ Does the most popular product always belong to the most popular company? Why or why not?
+ Does the lack of quantity turn customers away from that product? Does it make them turn away from the company?
+ Which types of products were most popular for each company?
+ Which company did better in the good economy? In the bad one?
+ Did one company stand on top in both good and bad economies? If so, which one?
+ */
--- DB/src/Gateway.java
+++ DB/src/Gateway.java
@@ -1,14 +1,15 @@
+package db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
-
+import java.util.Vector;
 
 //access to SQLite database
 class Gateway {
-	//use map over array or list - don't need ordering - only types and specifications
 	static Connection con;
 	static Statement stmt;
 	public Gateway(){}
@@ -19,67 +20,152 @@ class Gateway {
 		stmt=con.createStatement();
 	}
 	
-	//creates table
-	public static void createTable(String table_name, String... specs) throws SQLException{
-		String comm="CREATE TABLE IF NOT EXISTS EMPLOYEE (";
-		comm+="Fname VARCHAR(255), "+
-				"Minit VARCHAR(255) "+
-				"Lname VARCHAR(255) "+
-				"Ssn INT "+
-				"Bdate TEXT "+
-				"Address VARCHAR(255) "+
-				"Sex VARCHAR(255) "+
-				"Salary INT "+
-				"Super_ssn INT "+
-				"Dno INT )";
+	//creates tables
+	public static void createTables() throws SQLException{
+		String comm="CREATE TABLE IF NOT EXISTS COMPANY (";
+		comm+="Company_ID INT, "+
+				"Company_name VARCHAR(255), " +
+				"Profit INT )";
 
 		System.out.println(comm);
 		stmt.executeUpdate(comm);
 		
-		comm="CREATE TABLE IF NOT EXISTS DEPARTMENT (";
-		comm+="Dname VARCHAR(255), "+
-				"Dnumber INT "+
-				"Msg_ssn INT "+
-				"Msg_start_date TEXT )";
+		comm="CREATE TABLE IF NOT EXISTS PRODUCT (";
+		comm+="Brand_name VARCHAR(255), "+
+				"Holder_ID INT, " +
+				"Price INT, "+
+				"Product_type VARCHAR(255), "+
+				"Rating INT ) ";
 		
-		comm="CREATE TABLE IF NOT EXISTS PROJECT (";
-		comm+="Pname VARCHAR(255), "+
-				"Pnumber INT "+
-				"Plocation VARCHAR(255) "+
-				"Dnum INT )";
+		
+		System.out.println(comm);
+		stmt.executeUpdate(comm);
+		
+		comm="CREATE TABLE IF NOT EXISTS CUSTOMER (";
+		comm+="Customer_ID INT, "+
+				"Budget INT )";
 				
-		comm="CREATE TABLE IF NOT EXISTS DEPENDENT (";
-		comm+="Essn INT, "+
-				"Dependent_name VARCHAR(255) "+
-				"Sex CHARACTER(20) "+
-				"Bdate TEXT "+
-				"Relationship VARCHAR(255) )";
 		System.out.println(comm);
 		stmt.executeUpdate(comm);
 		
 	}
-	
-	
-	//dangerous 
+	//drops tables
 	static void dropTable(String table_name) throws SQLException{
 		stmt.executeUpdate("DROP TABLE "+table_name);
+		
 	}
-	
-	static void addCompany() throws SQLException(){
-		String comm="INSERT INTO COMPANY VALUES";
+	//adds a company
+	static void addCompany(int company_id, String company_name, int profit) throws SQLException {
+		String comm="INSERT INTO COMPANY (Company_ID, Company_name, Profit) VALUES ("+company_id+", '"+company_name+"', "+profit+")";
+		stmt.executeUpdate(comm);
 	}
-	
-	//gets a table of data in the form of a hash map where id=spec type and other column=spec
-	public static HashMap<String,String> getData(String table_name,String spec) throws SQLException{
-		ResultSet results=("".equals(spec)) ? stmt.executeQuery("SELECT * FROM "+table_name) : 
-			stmt.executeQuery("SELECT * FROM "+table_name+" WHERE ID='"+spec+"'");
-		HashMap<String,String> map=new HashMap<String,String>();
-		while(results.next()){
-			String key=results.getString(0);
-			String value=results.getString(1);
-			map.put(key,value);
+	//adds a product
+	static void addProduct(String brand_name, int holder_id, int price, String product_type, int rating) throws SQLException {
+		String comm="INSERT INTO PRODUCT (Brand_name, Holder_ID, Price, Product_type, Rating) VALUES ('"+brand_name+"', "+holder_id+", "+price+", '"+product_type+"', "+rating+")";
+		stmt.executeUpdate(comm);
+	}
+	//adds a customer
+	static void addCustomer(int customer_id, int budget) throws SQLException {
+		String comm="INSERT INTO CUSTOMER VALUES ("+customer_id+", "+budget+")";
+		stmt.executeUpdate(comm);
+	}
+	//raises or lowers a company's profit
+	static void updateCompanyProfit(int id, int amount, int profit) throws SQLException{
+		int sum=amount+profit;
+		String comm="UPDATE COMPANY SET Profit="+sum+" WHERE Company_ID="+id;
+		stmt.executeUpdate(comm);
+	}
+	//raises or lowers a product's price
+	static void updateProductPrice(int id, int percent_slash) throws SQLException{
+		ResultSet results=stmt.executeQuery("SELECT Price FROM PRODUCT WHERE Holder_ID="+id);
+		
+		results.next();
+		int price=results.getInt(1);
+		
+		int new_price=price/percent_slash;
+		String comm="UPDATE PRODUCT SET Price="+new_price+" WHERE Holder_ID="+id;
+		stmt.executeUpdate(comm);
+	}
+	//raises or lowers a company's profit
+	static void updateCustomerBudget(int id, int amount, int budget) throws SQLException{
+		
+		int difference=budget-amount;
+		
+		if(difference>=0){
+			String comm="UPDATE CUSTOMER SET Budget="+difference+" WHERE Customer_ID="+id;
+			stmt.executeUpdate(comm);
 		}
-		results.close();
-		return map;
+		else{
+			System.out.println("Budget can't be set any lower.");
+		}
+	}
+	//selects all companies
+	static Vector<Vector<Object>> selectCompanies() throws SQLException{
+		Vector<Vector<Object>> companies=new Vector<Vector<Object>>();
+		
+		ResultSet company_results=stmt.executeQuery("SELECT * FROM COMPANY");
+
+		while(company_results.next()){
+			Vector<Object> attributes=new Vector<Object>();
+			String company_name=company_results.getString("Company_name");
+			int company_id=company_results.getInt("Company_ID");
+			int profit=company_results.getInt("Profit");
+			
+			attributes.add(company_name);
+			attributes.add(company_id);
+			attributes.add(profit);
+			
+			companies.add(attributes);
+		}
+		company_results.close();
+		return companies;
+	}
+	//selects all products of a product type
+	static Vector<Vector<Vector<Object>>> selectProducts(Vector<String> product_types) throws SQLException{
+		Vector<Vector<Vector<Object>>> products=new Vector<Vector<Vector<Object>>>();
+		
+		for(int i=0;i<product_types.size();++i){
+			ResultSet product_results=(stmt.executeQuery("SELECT * FROM PRODUCT WHERE Product_type='"+product_types.get(i)+"'"));
+			products.add(new Vector<Vector<Object>>());
+
+			while(product_results.next()){
+				//products.get(i).add(new Vector<Object>());
+				Vector<Object> attributes=new Vector<Object>();
+				String brand_name=product_results.getString("Brand_name");
+				int product_id=product_results.getInt("Holder_ID");
+				int price=product_results.getInt("Price");
+				String product_type=product_results.getString("Product_type");
+				int rating=product_results.getInt("Rating");
+				
+				attributes.add(brand_name);
+				attributes.add(product_id);
+				attributes.add(price);
+				attributes.add(product_type);
+				attributes.add(rating);
+				
+				products.get(i).add(attributes);
+			}
+			product_results.close();
+		}
+		return products;
+	}
+	//selects all customers
+	static Vector<Vector<Object>> selectCustomers() throws SQLException{
+		Vector<Vector<Object>> customers=new Vector<Vector<Object>>();
+		
+		ResultSet customer_results=stmt.executeQuery("SELECT * FROM CUSTOMER");
+		
+		while(customer_results.next()){
+			Vector<Object> attributes=new Vector<Object>();
+			int customer_id=customer_results.getInt("Customer_ID");
+			int budget=customer_results.getInt("Budget");
+			
+			attributes.add(customer_id);
+			attributes.add(budget);
+			
+			customers.add(attributes);
+		}
+		customer_results.close();
+		return customers;
 	}
 }
